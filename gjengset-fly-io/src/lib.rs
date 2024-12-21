e262a698ec197bf76f434ad3a40044a48910c6b6
Oliver Hill <oliverhi@umich.edu>
2024-12-21 15:37:37 -0500

    splitting

--- gjengset-fly-io/src/lib.rs
+++ gjengset-fly-io/src/lib.rs
@@ -1,97 +1,48 @@
 use std::io::StdoutLock;
-use std::io::Write;
 
-use anyhow::{bail, Context};
+use anyhow::Context;
+use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Body {
+pub struct Body<Payload> {
     #[serde(rename = "msg_id")]
-    id: Option<usize>,
-    in_reply_to: Option<usize>,
+    pub id: Option<usize>,
+    pub in_reply_to: Option<usize>,
 
     #[serde(flatten)]
-    payload: Payload,
+    pub payload: Payload,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
-enum Payload {
-    Init {
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    InitOk,
-    Echo {
-        echo: String,
-    },
-    EchoOk {
-        echo: String,
-    },
-}
-
-pub struct EchoNode {
-    id: usize,
-}
-
-impl EchoNode {
-    pub fn step(&mut self, input: Message, output: &mut StdoutLock) -> anyhow::Result<()> {
-        match input.body.payload {
-            Payload::Init { .. } => {
-                let reply = Message {
-                    src: input.dst,
-                    dst: input.src,
-                    body: Body {
-                        id: Some(self.id),
-                        in_reply_to: input.body.id,
-                        payload: Payload::InitOk,
-                    },
-                };
-
-                serde_json::to_writer(&mut *output, &reply)
-                    .context("serialize response to init")?;
-                output.write_all(b"\n").context("write trailing newline")?;
-                self.id += 1;
-            }
-            Payload::InitOk => bail!("received init_ok message"),
-            Payload::Echo { echo } => {
-                let reply = Message {
-                    src: input.dst,
-                    dst: input.src,
-                    body: Body {
-                        id: Some(self.id),
-                        in_reply_to: input.body.id,
-                        payload: Payload::EchoOk { echo },
-                    },
-                };
-
-                serde_json::to_writer(&mut *output, &reply)
-                    .context("serialize response to init")?;
-                output.write_all(b"\n").context("write trailing newline")?;
-                self.id += 1;
-            }
-            Payload::EchoOk { .. } => {}
-        }
-
-        Ok(())
-    }
+pub struct Init {
+    pub node_id: String,
+    pub node_ids: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Message {
-    src: String,
+pub struct Message<Payload> {
+    pub src: String,
     #[serde(rename = "dest")]
-    dst: String,
-    body: Body,
+    pub dst: String,
+    pub body: Body<Payload>,
 }
 
-fn main() -> anyhow::Result<()> {
+pub trait Node<Payload> {
+    fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()>;
+}
+
+pub fn main_loop<S, Payload>(mut state: S) -> anyhow::Result<()>
+where
+    S: Node<Payload>,
+    Payload: DeserializeOwned,
+{
     let stdin = std::io::stdin().lock();
     let mut stdout = std::io::stdout().lock();
 
-    let mut state = EchoNode { id: 0 };
-    let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message>();
+    let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message<Payload>>();
     for input in inputs {
         let input = input.context("Maelstrom input from STDIN could not be deserialized")?;
         state
