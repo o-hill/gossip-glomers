bda7f65c2d91b128a654385f1c92979a4159568f
Oliver Hill <oliverhi@umich.edu>
2024-12-27 13:45:24 -0500

    multi node kafka

--- fly-io/scripts/kafka.sh
+++ fly-io/scripts/kafka.sh
@@ -1,2 +1,5 @@
 cargo build --bin kafka
-maelstrom/maelstrom test -w kafka --bin target/debug/kafka --node-count 1 --concurrency 2n --time-limit 20 --rate 1000
+# 5a (single node)
+# maelstrom/maelstrom test -w kafka --bin target/debug/kafka --node-count 1 --concurrency 2n --time-limit 20 --rate 1000
+# 5b (multi node)
+maelstrom/maelstrom test -w kafka --bin target/debug/kafka --node-count 2 --concurrency 2n --time-limit 20 --rate 1000
